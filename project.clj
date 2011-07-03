faefd9e1bf8863273a3e2e479d5753ae7806c99d
Eric Normand <ericwnormand@gmail.com>
2011-07-03 00:26:37 -0300

    Cleanup + increment project version

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject clj-peg "0.0.2"
+(defproject clj-peg "0.1.0"
   :description "clj-peg is a PEG parser written in Clojure.  It can parse any sequence of data types, including lists and vectors."
-  :dependencies [[org.clojure/clojure "[1.2.0,)"]]
+  
+  :dependencies     [[org.clojure/clojure  "[1.2.0,)"]]
+  
   :dev-dependencies [[leiningen/lein-swank "[1.1.0,)"]
-		     [swank-clojure "[1.2.0,)"]])
+                     [swank-clojure        "[1.2.0,)"]])
