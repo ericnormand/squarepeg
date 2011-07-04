7f20329fb904983373a9bbf881ade9e23fda7a0d
Eric Normand <ericwnormand@gmail.com>
2011-07-03 21:27:16 -0300

    Made memoizing work. Also, context is passed. Modified the examples to work with the new stuff.

--- src/clj_peg/combinators.clj
+++ src/clj_peg/combinators.clj
@@ -1,84 +1,89 @@
 (ns clj-peg.combinators)
 
-;; A rule is a function [input bindings] -> {:i seq :b {symbol vec} :r vec}
-;;                                       OR {:fail str} to signal failure
+;; A rule is a function [input bindings context memo]
+;; -> {:i seq :b {symbol vec} :r vec :m memo}
+;; OR {:fail str :m memo} to signal failure
 ;; input is a seq of the rest of the input
-;; bindings is a map from symbol to vecs of output
+;; bindings is a map from keyword to value
+;; context is a map from keyword to value (immutable)
+;; memo is the memoization hash
 ;; :fail is a failure message for the user
 
 ;; define success and failure
-(defn fail [msg]
+(defn fail [msg memo]
   "Fail a rule call with the given msg"
-  {:fail msg})
+  {:fail msg :m memo})
 (defn failure? [x]
   "Is x a failure?"
   (:fail x))
 
-(defn succeed [return sreturn input bindings]
+(defn succeed [return sreturn input bindings memo]
   "Succeed a rule call."
-  {:i input :b bindings :r return :s sreturn})
+  {:i input :b bindings :r return :s sreturn :m memo})
 (defn success? [x]
   "Is x a success?"
   (not (failure? x)))
 
 (defn mkfn [f]
   "Create a function useful for calling a rule at the REPL."
-  (fn
+  (fn ff
     ([input]
-       (let [r (f input {})]
+       (ff input {}))
+    ([input context]
+       (let [r (f input {} context {})]
          (if (success? r)
            (:r r)
            (throw (RuntimeException. (:fail r))))))
-    ([input bindings]
-       (f input bindings))))
+    ([input bindings context memo]
+       (f input bindings context memo))))
 
 (defn mknot [rule]
   "Create a rule that fails if the next input matches rule and
 succeeds otherwise."
-  (fn not-fn [input bindings]
-    (let [r (rule input bindings)]
+  (fn not-fn [input bindings context memo]
+    (let [r (rule input bindings context memo)]
       (if (failure? r)
-        (succeed nil [] input bindings)
-        (fail (str "NOT failed"))))))
+        (succeed nil [] input bindings (:m r))
+        (fail (str "NOT failed") (:m r))))))
 
 (defn mkbind [rule var]
   "Create a rule that binds the return value of rule to var."
-  (fn [input bindings]
-    (let [r (rule input bindings)]
+  (fn [input bindings context memo]
+    (let [r (rule input bindings context memo)]
       (if (success? r)
-        (succeed (:r r) (:s r) (:i r) (assoc (:b r) var (:r r)))
+        (succeed (:r r) (:s r) (:i r) (assoc (:b r) var (:r r)) (:m r))
         r))))
 
 (defn mkpr [pr]
   "Create a rule that consumes one item from the input. If pr applied
 to that item returns true, the rule succeeds. Otherwise, fail."
-  (fn [input bindings]
+  (fn [input bindings context memo]
     (if (nil? (seq input))
-      (fail "End of input")
+      (fail "End of input" memo)
       (let [i (first input)]
         (if (pr i)
-          (succeed i [i] (rest input) bindings)
-          (fail (str i " does not match predicate.")))))))
+          (succeed i [i] (rest input) bindings memo)
+          (fail (str i " does not match predicate.") memo))))))
 
 (defn mkret [rule ret]
   "Create a rule that returns a value. The value is computed by ret,
 which is a function of a bindings map. The rule also binds the return
 value of rule to :ret."
-  (fn [input bindings]
-    (let [r (rule input bindings)]
+  (fn [input bindings context memo]
+    (let [r (rule input bindings context memo)]
       (if (success? r)
         (let [b (assoc (:b r) :ret (:r r))
-              v (ret b)]
-          (succeed v [v] (:i r) b))
+              v (ret b context)]
+          (succeed v [v] (:i r) b (:m r)))
         r))))
 
 (defn mknothing [rule]
   "Create a rule that succeeds, fails, and consumes just like rule but
 returns nothing."
-  (fn [input bindings]
-    (let [r (rule input bindings)]
+  (fn [input bindings context memo]
+    (let [r (rule input bindings context memo)]
       (if (success? r)
-        (succeed nil [] (:i r) (:b r))
+        (succeed nil [] (:i r) (:b r) (:m r))
         r))))
 
 ;; helper function to concatenate vecs
@@ -103,35 +108,35 @@ returns nothing."
 (defn mkcat [rule1 rule2]
   "Create a rule that matches rule1 followed by rule2. Returns a vec
 of all return values."
-  (fn [input bindings]
-    (let [r1 (rule1 input bindings)]
+  (fn [input bindings context memo]
+    (let [r1 (rule1 input bindings context memo)]
       (if (failure? r1)
         r1
-        (let [r2 (rule2 (:i r1) (:b r1))]
+        (let [r2 (rule2 (:i r1) (:b r1) context (:m r1))]
           (if (failure? r2)
             r2
             (let [[r s] (catreturns r1 r2)]
-              (succeed r s (:i r2) (:b r2)))))))))
+              (succeed r s (:i r2) (:b r2) (:m r2)))))))))
 
 (defn mkseq [rules]
   "Create a rule that matches all of rules in order. Returns a vec of
 the return values of each."
-  (reduce mkcat #(succeed nil [] %1 %2) rules))
+  (reduce mkcat #(succeed nil [] %1 %2 %4) rules))
 
 (defn mkeither [rule1 rule2]
   "Create a rule that tries to match rule1. If it succeeds, the rule
 succeeds. Otherwise, it calls rule2."
-  (fn [input bindings]
-    (let [r1 (rule1 input bindings)]
+  (fn [input bindings context memo]
+    (let [r1 (rule1 input bindings context memo)]
       (if (failure? r1)
-        (rule2 input bindings)
+        (rule2 input bindings context (:m r1))
         r1))))
 
 (defn mkalt [rules]
   "Create a rule which succeeds if one of rules succeeds."
   (cond
    (nil? (seq rules))
-   (constantly (fail "no rules to match"))
+   #(fail "no rules to match" %4)
 
    (nil? (next rules))
    (first rules)
@@ -142,41 +147,41 @@ succeeds. Otherwise, it calls rule2."
 (defn mkpred [f]
   "Create a rule which never returns a value or consumes input. It
 succeeds if f returns non-nil and fails otherwise. f is a function of
-input bindings."
-  (fn [input bindings]
-    (if (f bindings)
-      (succeed nil [] input bindings)
-      (fail "Failed to match predicate"))))
+input bindings and context."
+  (fn [input bindings context memo]
+    (if (f bindings context)
+      (succeed nil [] input bindings memo)
+      (fail "Failed to match predicate" memo))))
 
 (defn mkzom [rule]
   "Create a rule which matches rule consecutively as many times as
 possible. The rule never fails. Returns a seq of all matched values."
-  (fn [input bindings]
-    (loop [val [] input input bindings bindings]
-      (let [r (rule input bindings)]
+  (fn [input bindings context memo]
+    (loop [val [] input input bindings bindings memo memo]
+      (let [r (rule input bindings context memo)]
         (if (success? r)
-          (recur (vec-cat val (:s r)) (:i r) (:b r))
-          (succeed val val input bindings))))))
+          (recur (vec-cat val (:s r)) (:i r) (:b r) (:m r))
+          (succeed val val input bindings memo))))))
 
 (defn mkscope [rule]
   "Create a rule which contains the scope of the given rule. Bindings
 made in rule do not escape this rule's scope."
-  (fn [input bindings]
-    (let [r (rule input {})]
+  (fn [input bindings context memo]
+    (let [r (rule input {} context memo)]
       (if (success? r)
-        (succeed (:r r) (:s r) (:i r) bindings)
+        (succeed (:r r) (:s r) (:i r) bindings (:m r))
         r))))
 
 (defn mksub [rule]
   "Create a rule which applies a rule to a nested seq within the
 input."
-  (fn [input bindings]
+  (fn [input bindings context memo]
     (if (and (seq input) (sequential? (first input)))
-      (let [r (rule (first input) bindings)]
+      (let [r (rule (first input) bindings context memo)]
         (if (success? r)
-          (succeed (:r r) (:s r) (rest input) (:b r))
+          (succeed (:r r) (:s r) (rest input) (:b r) (:m r))
           r))
-      (fail "Input not a seq."))))
+      (fail "Input not a seq." memo))))
 
 (defn mk1om [rule]
   "Create a rule which matches rule as many times as possible but at
@@ -185,10 +190,10 @@ least once."
 
 (defn mkopt [rule]
   "Create a rule which matches rule or not, but never fails."
-  (fn [input bindings]
-    (let [r (rule input bindings)]
+  (fn [input bindings context memo]
+    (let [r (rule input bindings context memo)]
       (if (failure? r)
-        (succeed [] [] input bindings)
+        (succeed [] [] input bindings (:m r))
         r))))
 
 ;; literal matcher
@@ -201,6 +206,23 @@ l, succeed."
   "Create a rule which matches all of the characters of a String s in
 sequence."
   (mkseq (map mklit s)))
+
+(defn mkmemo [rule]
+  "Create a rule that memoizes the given rule."
+  (let [memoid (gensym)]
+    (fn [input bindings context memo]
+      (if memo
+        (let [key [memoid input bindings]
+              mv (memo key)]
+          (when (:print-hits context)
+            (println "hits: "(:hit memo) " miss: " (:miss memo)))
+          (if mv
+            (assoc mv :m (assoc memo :hit (inc (memo :hit 0))))
+            (let [r (rule input bindings context memo)]
+              (assoc r :m (assoc (:m r)
+                            key  (dissoc r :m)
+                            :miss (inc ((:m r) :miss 0)))))))
+        (rule input bindings context memo)))))
 
 ;; utilities
 
--- src/clj_peg/examples.clj
+++ src/clj_peg/examples.clj
@@ -2,39 +2,39 @@
   (:use [clj-peg.combinators :as peg]))
 
 ;; match 0 or more whitespace
-(def w** (mkzom whitespace))
+(def w** (mkscope (mkzom whitespace)))
 
 ;; match 0 or more whitespace but return nothing
 ;; (effectively ignoring whitespace)
-(def w* (mknothing (mkzom whitespace)))
+(def w* (mkscope (mknothing (mkzom whitespace))))
 
 ;; match a newline
-(def newline (mklit \newline))
+(def newline (mkscope (mklit \newline)))
 
 ;; match anything that is not a newline
-(def non-newline (mkseq [(mknot newline) anything]))
+(def non-newline (mkscope (mkseq [(mknot newline) anything])))
 
 ;; match clojure style single-line comments
-(def clj-comment (mkseq [(mklit \;) (mkzom non-newline) newline]))
+(def clj-comment (mkscope (mkmemo (mkseq [(mklit \;) (mkzom non-newline) (mkalt [newline end])]))))
 
 ;; match an integer and parse it
 ;; the (apply str ) is necessary at the moment to convert the seq of
 ;; chars back to a string
 ;; mkscope is important to preven the binding of :ret (from mkret) to
 ;; leak out to the calling rule
-(def integer (mkscope (mkret (mk1om digit) #(Integer/parseInt (apply str (:ret %))))))
+(def integer (mkscope (mkmemo (mkret (mk1om digit) (fn [b c] (Integer/parseInt (apply str (:ret b))))))))
 
 ;; match a + character
-(def plus (mklit \+))
+(def plus (mkscope (mklit \+)))
 
 ;; match a * character
-(def times (mklit \*))
+(def times (mkscope (mklit \*)))
 
 ;; match an open paren
-(def open (mklit \())
+(def open (mkscope (mklit \()))
 
 ;; match a close paren
-(def close (mklit \)))
+(def close (mkscope (mklit \))))
 
 ;; using the above rules, we can define a simple calculator
 
@@ -45,18 +45,18 @@
 ;; mutually or self-recursive rules
 ;; note that because we use #' and we have predeclared, the rules can
 ;; appear in any order
-(def term (mkscope (mkalt [(mkret (mkseq [open w* (mkbind #'sum :value) w* close]) :value)
-                           integer])))
+(def term (mkscope (mkmemo (mkalt [(mkret (mkseq [open w* (mkbind #'sum :value) w* close]) :value)
+                                   integer]))))
 
-(def product (mkscope (mkalt [(mkret (mkseq [(mkbind term :a) w* times w* (mkbind #'product :b)])
-                                     #(* (:a %) (:b %)))
-                              term
-                              ])))
+(def product (mkscope (mkmemo (mkalt [(mkret (mkseq [(mkbind term :a) w* times w* (mkbind #'product :b)])
+                                             (fn [b c] (* (:a b) (:b b))))
+                                      term
+                                      ]))))
 
-(def sum (mkscope (mkalt [(mkret (mkseq [(mkbind #'product :a) w* plus w* (mkbind #'sum :b)])
-                                 #(+ (:a %) (:b %)))
-                          #'product
-                          ])))
+(def sum (mkscope (mkmemo (mkalt [(mkret (mkseq [(mkbind #'product :a) w* plus w* (mkbind #'sum :b)])
+                                         (fn [b c] (+ (:a b) (:b b))))
+                                  #'product
+                                  ]))))
 
 ;; create a convenient function to call the rule with
 (def calc (mkfn #'sum))
@@ -70,15 +70,15 @@
 
 (def optimize- (mkfn #'expr))
 (defn optimize [e]
-  (optimize- [e]))
+  (optimize- [e] {:print-hits true}))
 
 ;; an expression is a (+) expr, a (*) expr, another fncall, a var, or
 ;; a number
-(def expr (mkscope (mkalt [(mksub #'addition)
-                           (mksub #'multiplication)
-                           #'fncall
-                           match-symbol
-                           match-number])))
+(def expr (mkscope (mkmemo (mkalt [(mksub #'addition)
+                                   (mksub #'multiplication)
+                                   (mksub #'fncall)
+                                   match-symbol
+                                   match-number]))))
 
 (defn separate [pr coll]
   [(filter pr coll)
@@ -86,30 +86,36 @@
 
 ;; an addition expression is a + followed by arguments
 ;; args will recursively optimize the arguments
-(def addition (mkscope (mkret (mkseq [(mklit '+) (mkbind args :args)])
-                              ;;find the literal numbers
-                              #(let [[nums other] (separate number? (:args %))
-                                     sum (apply + nums)]
-                                 (cond
-                                  ;; if there are only numbers, we
-                                  ;; don't need to add at runtime
-                                  (nil? (seq other))
-                                  sum
-                                  ;; if the sum is zero and only one
-                                  ;; other term, we don't need to add
-                                  (and (zero? sum) (nil? (rest other)))
-                                  (first other)
-                                  ;; if the sum is zero, just add the
-                                  ;; other terms
-                                  (zero? sum)
-                                  `(+ ~@other)
-                                  ;; otherwise, return the addition
-                                  :otherwise 
-                                  `(+ ~sum ~@other))))))
+(def addition (mkscope
+               (mkmemo
+                (mkret (mkseq [(mklit '+) (mkbind #'args :args)])
+                       ;;find the literal numbers
+                       (fn [b c]
+                         (let [[nums other] (separate number? (:args b))
+                               sum (apply + nums)]
+                           (cond
+                            ;; if there are only numbers, we
+                            ;; don't need to add at runtime
+                            (nil? (seq other))
+                            sum
+                            ;; if the sum is zero and only one
+                            ;; other term, we don't need to add
+                            (and (zero? sum) (nil? (rest other)))
+                            (first other)
+                            ;; if the sum is zero, just add the
+                            ;; other terms
+                            (zero? sum)
+                            `(+ ~@other)
+                            ;; otherwise, return the addition
+                            :otherwise 
+                            `(+ ~sum ~@other))))))))
 
-(def multiplication (mkscope (mkret (mkseq [(mklit '*) (mkbind args :args)])
-                              ;;find the literal numbers
-                              #(let [[nums other] (separate number? (:args %))
+(def multiplication (mkscope
+                     (mkmemo
+                      (mkret (mkseq [(mklit '*) (mkbind #'args :args)])
+                             ;;find the literal numbers
+                             (fn [b c]
+                               (let [[nums other] (separate number? (:args b))
                                      prod (apply * nums)]
                                  (cond
                                   ;; if there are only numbers, we
@@ -127,8 +133,13 @@
                                   `(* ~@other)
                                   ;; otherwise, return the addition
                                   :otherwise 
-                                  `(* ~prod ~@other))))))
-(def fncall match-list)
+                                  `(* ~prod ~@other))))))))
+
+(def fncall (mkscope
+             (mkmemo
+              (mkret (mkseq [(mkbind match-symbol :sym) (mkbind #'args :args)])
+                     (fn [b c]
+                       `(~(:sym b) ~@(:args b)))))))
 
 ;; match all args up to the end
-(def args (mkseq [(mkzom #'expr) end]))
+(def args (mkscope (mkmemo (mkseq [(mkzom #'expr) end]))))
